5df68d13b411ea304c8d44693221517b9ae5841a
Ernest Shefer <shef-er@ya.ru>
2018-07-07 15:29:08 +0500

    init

---
+++ app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+
+process.env.TOKEN = '63faee37962cac78da9a7993674d02fe945660986b0dd22f222a0beea76fac950047e52b04f86b1f0d501';
+
+const API = require('node-vk-bot-api');
+
+const bot = new API(process.env.TOKEN);
+
+bot.command('start', ({ reply }) => reply('This is start!'));
+bot.hears(/(car|tesla)/, ({ reply }) => reply('I love Tesla!'));
+bot.on(({ reply }) => reply('What?'));
+
+bot.listen();
